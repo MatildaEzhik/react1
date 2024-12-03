2f68730cb2f79b6bf05cd79c63f0c62e8e3ed347
arkanitochka <arkanitochka@gmail.com>
2024-12-03 16:28:55 +0500

    git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+function getDayInfo() {
+    const now = new Date();
+    const day = now.getDate();
+    const isEven = day % 2 === 0;
+
+    if (isEven) {
+        return `${now.toLocaleString('default', { month: 'long' })}, ${now.getFullYear()}`;
+    } else {
+        return `${now.toLocaleString('default', { weekday: 'long' })}, ${day}`;
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <h1>react homework #1</h1>
+            <h2>reversed String:</h2>
+            <p>{reverseString('React.js')}</p>
+            <h2>Day Info:</h2>
+            <p>{getDayInfo()}</p>
+        </div>
+    );
 }
 
 export default App;
